cc09c46a4a1c6c31d88f73f46d0b89b73c2779c7
alexvallcorba <alexgvallcorba@gmail.com>
2021-10-04 18:34:57 -0400

    Creating data for surfboards

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Custom.destroy_all
+Surfboard.destroy_all
+User.destroy_all
+
+
+@hydratop = Surfboard.create!(
+  name: 'Hydra Top', 
+  description: 'The HYDRA TOP_Grom is fun for kids transitioning from soft tops or oversized every level boards, but still struggling to consistently control and perform on a full HP Shortboard. It’s also become a very functional “grovel” board for some of our elite level kids. In areas plagued with consistently minuscule waves, the UberGrom is better choice for even higher level rippers. Meant to be ridden at the kids same hight, for modest skill level, gradually becoming shorter as the groms skills and hight grows.'
+,
+)
