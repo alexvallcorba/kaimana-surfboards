caba32ec1ca01c4a0472895bca55062c05098891
alexvallcorba <alexgvallcorba@gmail.com>
2021-10-11 09:35:23 -0400

    netlify issue resolved

--- client/src/services/api-config.js
+++ client/src/services/api-config.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseUrl = process.env.NODE_ENV === 'production' ? 'https://school-app-test.herokuapp.com/' : 'http://localhost:3000' 
+const baseUrl = process.env.NODE_ENV === 'production' ? 'https://kaimana-surfboards-api.herokuapp.com/' : 'http://localhost:3000' 
 ;
 
 const api = axios.create({
