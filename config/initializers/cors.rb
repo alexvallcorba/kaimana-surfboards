e4105685ccb04384d128075e1e431ea89b04c3e4
alexvallcorba <alexgvallcorba@gmail.com>
2021-10-11 09:29:56 -0400

    CORS issue

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'https://kaimana-surfboards.netlify.app'
 
     resource '*',
       headers: :any,
